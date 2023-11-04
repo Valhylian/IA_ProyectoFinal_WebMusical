d99b57181d9066c74e47a4002e5e4cd8dd387523
Valhylian <valeria44a@estudiantec.cr>
2023-11-04 00:24:36 -0600

    Carga Letra en el front

--- front_webMusical/src/App.tsx
+++ front_webMusical/src/App.tsx
@@ -4,25 +4,22 @@ function App() {
   const [idea, setIdea] = useState('');
   const [genero, setGenero] = useState('');
   const [melody, setMelody] = useState('');
+  const [composedSong, setComposedSong] = useState(''); // Estado para mostrar la canción generada
 
-
-  const [imageUrl, setImageUrl] = useState<string | null>(null);
-  const ideaTextareaRef = useRef<HTMLTextAreaElement | null>(null); // Ref para el campo de "idea"
-  const genreInputRef = useRef<HTMLInputElement | null>(null); // Ref para el campo de "genero"
-  const melodyTextareaRef = useRef<HTMLTextAreaElement | null>(null); // Ref para el campo de "melody"
+  const ideaTextareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const genreInputRef = useRef<HTMLInputElement | null>(null);
+  const melodyTextareaRef = useRef<HTMLTextAreaElement | null>(null);
 
   const handleStartComposition = () => {
     if (ideaTextareaRef.current) {
       const ideaValue = ideaTextareaRef.current.value;
-      const genreInput = document.getElementById('genero') as HTMLInputElement; // Añadir 'as HTMLInputElement'
+      const genreInput = document.getElementById('genero') as HTMLInputElement;
 
       if (genreInput) {
         const genreValue = genreInput.value;
         const jsonData = {
           question: `Necesito una letra original, de al menos 3 versos para una canción del género ${genreValue} con la siguiente idea: ${ideaValue}`,
         };
-        console.log(jsonData); // Imprime el objeto jsonData en la consola
-
 
         var myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
@@ -35,19 +32,19 @@ function App() {
           body: raw,
           redirect: 'follow' as RequestRedirect
         };
-        
 
         fetch("http://localhost:3000/chat-gpt-ai/mesagge", requestOptions)
           .then((response) => {
             if (response.ok) {
-              return response.json(); // Parsear la respuesta JSON
+              return response.json();
             } else {
               throw new Error('La solicitud no se pudo completar con éxito.');
             }
           })
           .then((responseJson) => {
-            console.log('Respuesta del servidor:', responseJson);
-            // Aquí puedes manejar la respuesta como desees
+            if (responseJson && responseJson[0] && responseJson[0].text) {
+              setComposedSong(responseJson[0].text); // Establece la canción generada
+            }
           })
           .catch((error) => {
             console.error('Error en la solicitud:', error);
@@ -60,34 +57,27 @@ function App() {
     }
   };
 
-
-
-
-
-
-
-
   return (
     <div className="bg-gray-900 min-h-screen text-white flex items-center justify-center">
       <div className="bg-gray-800 p-4 w-2/5 rounded-lg">
         <h1 className="text-3xl font-bold text-center my-2">Let's compose!</h1>
 
+       
         <div className="mt-4">
           <label htmlFor="genero" className="block text-sm font-medium text-gray-400">
             Genre:
           </label>
           <input
-            type="text" // Cambia el tipo a "text"
+            type="text"
             id="genero"
             name="genero"
             ref={genreInputRef}
             className="w-full bg-gray-700 text-gray-200 border border-gray-600 rounded-md py-2 px-3 mt-1"
             placeholder="Example: Rock, Soft, Cumbia..."
             value={genero}
-            onChange={(e) => setGenero(e.target.value)} // Asegúrate de cambiar setIdea a setGenero
+            onChange={(e) => setGenero(e.target.value)}
           />
         </div>
-
 
         <div className="mt-4">
           <label htmlFor="idea" className="block text-sm font-medium text-gray-400">
@@ -122,23 +112,26 @@ function App() {
         </div>
 
         <button
-          className="bg-blue-500 text-white font-medium py-2 px-4 rounded-md mt-4 hover:bg-blue-600"
+          className="bg-blue-500 text-white font-medium py-2 px-4 rounded-md mt-4 hover-bg-blue-600"
           onClick={handleStartComposition}
         >
           Compose song
         </button>
-        {/* Resto del contenido... */}
 
-        <div className="mt-4">
-          <div className="bg-gray-700 h-32 rounded-md mt-4 image-container">
-            <img
-              src={imageUrl || ''}
-              alt="Imagen del servidor"
-              className="w-full h-full object-cover"
-            />
-          </div>
+         {/* Agrega un campo de texto para mostrar la canción generada */}
+         <div className="mt-4">
+          <label htmlFor="composedSong" className="block text-sm font-medium text-gray-400">
+            Composed Song:
+          </label>
+          <textarea
+            id="composedSong"
+            name="composedSong"
+            rows={6} // Ajusta la cantidad de filas según tu preferencia
+            className="w-full bg-gray-700 text-gray-200 border border-gray-600 rounded-md py-2 px-3 mt-1"
+            value={composedSong} // Muestra la canción generada
+            readOnly // Hace que el campo sea de solo lectura
+          />
         </div>
-
 
       </div>
     </div>
