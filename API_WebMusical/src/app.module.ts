6078460431546e9b2647a6fc1966bb72d54d5dde
Valhylian <valeria44a@estudiantec.cr>
2023-11-03 21:12:31 -0600

    Respaldo, esta funcionando

--- API_WebMusical/src/app.module.ts
+++ API_WebMusical/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { StableDiffusionIntegrationModule } from './stable-diffusion-integration/stable-diffusion-integration.module';
 import { ConfigModule } from '@nestjs/config';
 import { MusicGenController } from './music-gen/music-gen.controller';
@@ -12,7 +11,7 @@ dotenv.config();
 
 @Module({
   imports: [StableDiffusionIntegrationModule, ConfigModule.forRoot(), ChatGptAiModule],
-  controllers: [AppController, MusicGenController],
-  providers: [AppService],
+  controllers: [MusicGenController],
+  providers: [],
 })
 export class AppModule {}
--- API_WebMusical/src/main.ts
+++ API_WebMusical/src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import { join } from 'path';
+const path = require('path')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
   await app.listen(3000);
 }
 bootstrap();
